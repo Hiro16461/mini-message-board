1d11dcfd260748b737840730b350b7d658a64a37
Hiro16461 <158548582+Hiro16461@users.noreply.github.com>
2026-07-05 07:09:14 +0900

    Create routes folder and files

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/indexRouter')
+const newMessageRouter = require('./routes/newMessageRouter')
+
+app.use('/', indexRouter)
+app.use('/new', newMessageRouter)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (error) => {
+	if (error) {
+		throw error;
+	}
+	console.log(`Mini message board - listening on port ${PORT}`);
+});
---
+++ routes/indexRouter.js
@@ -0,0 +1,6 @@
+const { Router } = require('express')
+const indexRouter = Router();
+
+indexRouter.get('/', (req, res) => res.send('Index Route'));
+
+module.exports = indexRouter;
