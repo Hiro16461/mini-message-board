9aadb8d9697ad8c15da0c3ad375c63ec27efc39c
Hiro16461 <158548582+Hiro16461@users.noreply.github.com>
2026-07-05 07:45:59 +0900

    Create index route

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,23 @@
-const { Router } = require('express')
+const { Router } = require('express');
 const indexRouter = Router();
 
-indexRouter.get('/', (req, res) => res.send('Index Route'));
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
+indexRouter.get('/', (req, res) =>
+	res.render('index', {
+		messages: messages,
+	}),
+);
 
 module.exports = indexRouter;
